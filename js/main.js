42a8c0039291d5c733275114225c46a1b4381c95
Seokyeong-Park <melona1221@naver.com>
2022-12-14 01:18:00 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        anchor: ['section1', 'section2', 'section3', 'section4', 'section5'],
+        navigation:true,
+        navigationPosiotion:'right'
+    });
+});
